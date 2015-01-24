339e82357dd41f7206ef9109e550ad2f09fd1bde
AndresCW <andres@cpsingenieria.co>
2015-01-24 17:36:46 -0500

    Se agrego un template basico, sin usar el shortcut de render

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import RequestContext, loader
+
+from blog.models import Post
 
 def blog_index(request):
-	return HttpResponse('Indice del blog :)')
+	posts = Post.objects.all()
+	template = loader.get_template('blog/blog_index.html')
+	context = RequestContext(request, {
+		'posts':posts,
+	})
+	return HttpResponse(template.render(context))
 
 def post_detail(request, post_title):
 	response = "Estas buscando el post titulado: %s."
