3490fb0ed05eb794a732c5f7552ddd8067033cf9
AndresCW <andres@cpsingenieria.co>
2015-01-24 21:37:47 -0500

    Mejoradas las urls hardcodeadas

--- CloudSCADA/urls.py
+++ CloudSCADA/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'CloudSCADA.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^blog/', include('blog.urls')),
-    url(r'^faq/', include('doubts.urls')),
+    url(r'^blog/', include('blog.urls', namespace="blog")),
+    url(r'^faq/', include('doubts.urls', namespace="doubts")),
     url(r'^admin/', include(admin.site.urls)),
 )
