a323a5ceef951112cf541c4c5ae3cca3fb5edc47
AndresCW <andres@cpsingenieria.co>
2015-01-26 00:03:36 -0500

    Creado el primer test;  verifica que el metodo Question.was_published_recently() funcione

--- doubts/tests.py
+++ doubts/tests.py
@@ -1,3 +1,16 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+from doubts.models import Question
+
+class QuestionMethodTests(TestCase):
+
+	def test_was_published_recently_with_future_question(self):
+		"""
+		was_published_recently() deberia retornar False para las preguntas
+		cuya pub_date sea en el futuro
+		"""
+		future_time = timezone.now() + datetime.timedelta(days=30)
+		future_question = Question(pub_date=future_time)
+		self.assertEqual(future_question.was_published_recently(), False) 
